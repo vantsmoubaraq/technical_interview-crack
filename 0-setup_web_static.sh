a0f61653bd4274dce6b4e40075e2c32765f09d63
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-01-23 17:19:33 +0300

    deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,12 @@
-#!/usr/bin/bash
-#sets up your web servers for the deployment of web_static
+#!/usr/bin/env bash
+# sets up the web servers for the deployment of web_static
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sed -i "/server_name _;/a\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\t\n}" /etc/nginx/site-available/default
+sudo sed -i '/server_name _;/a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start

dd867bc59cebdb42c33926344de2bfcdaf9d8a40
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-02-01 20:01:12 +0300

    mysql

---
+++ mysql/4-replica.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+echo "server-id	= 1" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "log_bin         = /var/log/mysql/mysql-bin.log" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "binlog_do_db	= tyrell_corp" >>  /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "relay-log       = /var/log/mysql/mysql-relay-bin.log" >> /etc/mysql/mysql.conf.d/mysqld.cnf

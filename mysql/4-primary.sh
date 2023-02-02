a975f522b682e597fb5c9d3b387d8af52f2d7dba
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-02-02 18:05:29 +0300

    mysql

--- mysql/4-primary.sh
+++ mysql/4-primary.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #Run exclusively on the master, web-01
 #How to run: sudo ./4-primary.sh
-sed -i 's/bind-address  = 127.0.0.1/#&/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i 's/bind/#&/' /etc/mysql/mysql.conf.d/mysqld.cnf
 echo "server-id	= 1" >> /etc/mysql/mysql.conf.d/mysqld.cnf
 echo "log_bin         = /var/log/mysql/mysql-bin.log" >> /etc/mysql/mysql.conf.d/mysqld.cnf
 echo "binlog_do_db	= tyrell_corp" >>  /etc/mysql/mysql.conf.d/mysqld.cnf

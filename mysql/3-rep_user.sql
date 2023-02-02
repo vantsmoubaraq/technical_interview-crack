4d7bd9e8f100cf155eb6cd240f094a50dcf2e792
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-02-02 10:59:55 +0300

    mysql

--- mysql/3-rep_user.sql
+++ mysql/3-rep_user.sql
@@ -1,5 +1,6 @@
 -- create replica user
 -- change the password to one of your preference in line 5
+-- Run exclusively on master, web-01
 -- How to run: cat 3-rep_user.sql | mysql -u root -p
 
 CREATE USER "replica_user"@"%" IDENTIFIED BY "password";
